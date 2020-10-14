630c63c914d900af3800ae658da10f123c51f823
CloudSmall <72876152+CloudSmall@users.noreply.github.com>
2020-10-14 19:30:02 +0300

    Added 2 files with my first program

---
+++ About_Yourself.java
@@ -0,0 +1,7 @@
+public class About_Yourself {
+    public static void main(String[] args) {
+        System.out.println( "Hello world! My name is Inna. I am work as a QA engineer. My area of expertise includes testing web and mobile application");
+        System.out.println("Hello,Inna!");
+    }
+
+}
---
+++ My_Initials.java
@@ -0,0 +1,16 @@
+public class My_Initials {
+    public static void main(String[] args) {
+        System.out.println("My initials are:");
+        System.out.println();
+        System.out.println("           MM                MM     II      NN           NN ");
+        System.out.println("           MM MM          MM MM     II      NN NN        NN ");
+        System.out.println("           MM    MM    MM    MM     II      NN  NN       NN ");
+        System.out.println("           MM       MM       MM     II      NN   NN      NN ");
+        System.out.println("           MM                MM     II      NN    NN     NN ");
+        System.out.println("           MM                MM     II      NN     NN    NN ");
+        System.out.println("           MM                MM     II      NN      NN   NN ");
+        System.out.println("           MM                MM     II      NN       NN  NN ");
+        System.out.println("           MM                MM     II      NN        NN NN ");
+        System.out.println();
+    }
+}
